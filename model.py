790718db7a82edd2628a14aeb1606a08162f3f97
Friedrich Lindenberg <friedrich@pudo.org>
2015-01-02 21:42:00 +0100

    begin sketching out a better model

--- model.py
+++ model.py
@@ -1,2 +1,66 @@
 
 
+class Dataset(object):
+    
+    def __init__(self):
+        self.id = None
+        self.label = None
+        self.model = None
+        self.fields = None
+
+
+class Field(object):
+    pass
+
+
+class Model(object):
+    
+    def __init__(self):
+        self.variables = []
+        self.measures = []
+        self.dimensions = []
+
+
+class AttributeType(object):
+
+    def __init__(self):
+        self.id = None
+        self.label = None
+
+
+class Attribute(object):
+    
+    def __init__(self):
+        self.type = None
+        self.mapping = None
+
+
+class Variable(object):
+    pass
+
+
+class Measure(Variable):
+    
+    def __init__(self):
+        self.attribute = None
+
+
+class Level(object):
+
+    def __init__(self):
+        self.id = None
+        self.label = None
+        self.attributes = None
+        self.hierarchies = None
+
+
+class Hierarchy(set):
+    
+    def __init__(self):
+        pass
+
+
+class Dimension(Variable):
+    pass
+
+
